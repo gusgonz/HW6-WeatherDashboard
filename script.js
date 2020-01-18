871c21e16414097a1f1371123a45d1196225a4fa
GusGo <gus.gonzalez.15@gmail.com>
2020-01-18 00:39:02 -0500

    searchbar wip

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // apikey 7ed6e592c87c5c5e7c239aee3ee410d9
+
+// Algolia third-party api
+var placesAutocomplete = places({
+	appId: 'YHSNLEDJ0A',
+	apiKey: '3edc49d1e984d739c114787534c196c9',
+	container: document.querySelector('#address-input')
+});
