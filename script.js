0769c9e7bc22046107f153464a7d597bc7b62f05
GusGo <gus.gonzalez.15@gmail.com>
2020-01-18 16:07:15 -0500

    little code reformatting

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// apikey 7ed6e592c87c5c5e7c239aee3ee410d9
+// open weather apikey 7ed6e592c87c5c5e7c239aee3ee410d9
 
-// Algolia third-party api
+// Algolia third-party api for the searchbar
 var placesAutocomplete = places({
 	appId: 'plHJ3V77N7R0',
 	apiKey: '15c839632d2df1c8823fe9d3cd8988f5',
